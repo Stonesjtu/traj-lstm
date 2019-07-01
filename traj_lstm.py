247fdbc03ab8e7ffe806962441c458491b746953
Kaiyu Shi <skyisno.1@gmail.com>
2019-07-01 22:40:29 +0800

    Add batch_first flag and layernorm and hidden state init

--- traj_lstm.py
+++ traj_lstm.py
@@ -1,13 +1,16 @@
 import torch
 from torch import nn
 
+
 class TrajLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers=1, dropout=0):
+    def __init__(self, input_size, hidden_size,
+                 num_layers=1, dropout=0, batch_first=False):
         super().__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.dropout = dropout
+        self.batch_first = batch_first
 
         time_lstm = [nn.LSTM(input_size, hidden_size)]
         for _ in range(num_layers - 1):
@@ -15,32 +18,44 @@ class TrajLSTM(nn.Module):
         self.time_lstm = nn.Sequential(*time_lstm)
         self.drop = nn.Dropout(dropout)
         self.depth_lstm = nn.LSTM(hidden_size, hidden_size)
-        # self.layernorm = torch.nn.LayerNorm(hidden_size)
+        self.layernorms = nn.ModuleList(
+            [torch.nn.LayerNorm(hidden_size) for _ in range(num_layers)]
+        )
 
     def forward(self, input, hidden=None):
+        if self.batch_first:
+            input = input.transpose(0, 1)
         time_output = input
         time_results = []
         if hidden is None:
             hidden = [None for _ in self.time_lstm]
+        else:
+            all_h, all_c = hidden
+            hidden = [(h.unsqueeze(0), c.unsqueeze(0))
+                      for h, c in zip(all_h, all_c)]
         next_hidden = []
         next_cell = []
-        all_h, all_c = hidden
-        for lstm, h, c in zip(self.time_lstm, all_h, all_c):
+        for lstm, state, layernorm in zip(self.time_lstm, hidden, self.layernorms):
 
-            state = (h.unsqueeze(0), c.unsqueeze(0))
-            time_output, (next_h, next_c) = lstm(time_output, state)  # seq X bs X hidden
+            # seq X bs X hidden
+            time_output = layernorm(time_output)
+            time_output, (next_h, next_c) = lstm(time_output, state)
             next_hidden.append(next_h)
             next_cell.append(next_c)
             time_output = self.drop(time_output)
-            # time_output = self.layernorm(time_output)
             time_results.append(time_output)
 
         time_results = torch.stack(time_results)  # depth X seq X bs X hidden
         depth, seq, bs, hidden = time_results.size()
-        depth_results, _ = self.depth_lstm(time_results.view(depth, seq*bs, hidden))
-        output = depth_results[-1]  # seq*bs X hidden
+        _, (depth_h, depth_c) = self.depth_lstm(
+            time_results.view(depth, seq*bs, hidden)
+        )
+        output = depth_c  # seq*bs X hidden
         output = output.view(seq, bs, hidden) + time_output
-        next_state = (torch.stack(next_hidden[::-1]).squeeze(1), torch.stack(next_cell[::-1]).squeeze(1))
+        next_state = (torch.stack(next_hidden[::-1]).squeeze(1),
+                      torch.stack(next_cell[::-1]).squeeze(1))
+        if self.batch_first:
+            output = output.transpose(0, 1)
         return output, next_state
 
     def flatten_parameters(self):
@@ -48,11 +63,11 @@ class TrajLSTM(nn.Module):
             lstm.flatten_parameters()
         self.depth_lstm.flatten_parameters()
 
+
 class HuhuLSTM(TrajLSTM):
     def forward(self, input, hidden=None):
         time_output = input
         hidden = [None for _ in self.time_lstm]
-        next_hidden = []
         depth_next = None
         for lstm, cur_hidden in zip(self.time_lstm, hidden):
             time_output, _ = lstm(time_output, None)  # seq X bs X hidden
@@ -61,15 +76,18 @@ class HuhuLSTM(TrajLSTM):
                 time_output.view(1, -1, self.hidden_size),
                 depth_next,
             )
-            # use the output of time_lstm as the input of the next layer of time lstm
+            # use the output of time_lstm as the input of the next layer
+            # of time lstm
             time_output = depth_out.view_as(time_output)
 
         return time_output, None
+
 
 def test_traj_lstm():
     traj_lstm = TrajLSTM(256, 256, 6).cuda()
     fake_input = torch.zeros(30, 30, 256).cuda()
     print(traj_lstm(fake_input).mean().item())
+
 
 if __name__ == '__main__':
     test_traj_lstm()
